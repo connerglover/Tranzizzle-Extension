61ee2155787a045fa17fa7be3746c60de190b6b8
Conner <110003454+ConnerConnerConner@users.noreply.github.com>
2023-04-29 18:28:59 -0500

    Create script.js

---
+++ script.js
@@ -0,0 +1,3 @@
+if (!(window.location.href.includes("https://www.gizoogle.net/tranzizzle.php?search="))) {
+    window.location.replace("https://www.gizoogle.net/tranzizzle.php?search=" + window.location.href)
+}
